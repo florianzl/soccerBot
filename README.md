8fed3e76febd744696c25a6ecb780db863067513
florianzl <f.zitlau@web.de>
2022-02-03 01:14:07 +0100

    alles ordentlich gemacht

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # soccerBot
-Main Program of Bohlebots ToTheMoon soccerbot
--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,66 @@
 /*
-  * OHNE KICKER
-*/
+ * OHNE KICKER
+ */
 
 #include <Arduino.h>
+
 #include "functions.h"
 
 int modus;
 
 void setup() {
   modus = 0;
-  bot.setSoccer(true);
-  bot.setPixy(true);
-  bot.init();
-  Serial.print("start");
+  startBot();
 }
 
 void loop() {
   bot.warte(5);
 
   if (bot.button(0, 1)) {
-    startBot();
+    // kicker test
   }
 
-  if (bot.button(0, 2))
-    modus = 2;
-
-  if (bot.button(7, 1))
+  else if (bot.button(0, 2))
     modus = 1;
 
-  if (bot.button(7, 2)) {
+  else if (bot.button(7, 1))
+    modus = 2;
+
+  else if (bot.button(7, 2)) {
     stopBot();
     modus = 0;
   }
 
-  if (modus == 2) {
-    bot.readPixy();
+  // wenn ball mind. richtung 3 ist fährt bot hinter ball, sonst zum ball
+  if (modus == 1) {
     if (bot.ball() > 0) {
-
-      if (!bot.readLightSensor()) {
-
-          
-        //kicker
-
-        //fahre ins tor
-        //höhere geschwindigkeit
+      if (bot.hasBall()) {
+        // TODO kicker
         bot.drive(0, 30, rotate());
-      }
-
-      //ball nicht in Ballschranke
-      else {
-
-        if ((bot.ballDirection() < 3 && bot.ballDirection() > 0) || (bot.ballDirection() > -3 && bot.ballDirection() < 0)) {
-
+      } else {
+        if (bot.ballDirection() < 3 && bot.ballDirection() > -3)
           bot.drive(0, 30, turnToBall());
-        }
-
-        else {
-
-          
+        else
           bot.drive(directionBehindBall() / 2, 30, rotate());
-        }
       }
-    }
-    else {
-      bot.drive(0,0, rotate());
-    }
+    } else
+      bot.drive(0, 0, rotate());
   }
 
+  else if (modus == 2) {
+    if (bot.ball()) {
+      if (bot.hasball()) {
+      } else {
+        bot.drive(directionBehindBall / 2, 30, rotate());
+      }
+    } else
+      bot.drive(0, 0, rotate());
+  }
 
-//testen der rotation
-  if (modus == 1) {
-    bot.drive(0,30,0);
-    //bot.drive(0,30,rotate());
+  // testen der rotation
+  else if (modus == 10) {
+    bot.drive(0, 30, 0);
+    // bot.drive(0,30,rotate());
     Serial.println(rotate());
   }
 }
